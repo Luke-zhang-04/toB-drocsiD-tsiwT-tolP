18059c59c02309ec78006d6d294f7ec6f0c0cefd
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-01-02 06:12:30 +0000

    remove @ everyone "feature"

--- delete.mjs
+++ delete.mjs
@@ -12,32 +12,37 @@ client.login(process.env.AUTHTOKEN)
 /** @type {{[key: string]: import('discord.js').Message}} */
 const messageMap = {}
 
-client.on("messageUpdate", async (message) => {
-    if (message.author.id === botId && message.deletable) {
-        try {
-            await Promise.allSettled([message.delete(), messageMap[message.id]?.delete()])
+// client.on("messageUpdate", async (message) => {
+//     if (message.author.id === botId && message.deletable) {
+//         try {
+//             await Promise.allSettled([message.delete(), messageMap[message.id]?.delete()])
 
-            delete messageMap[message.id]
-        } catch {}
-    }
-})
+//             delete messageMap[message.id]
+//         } catch {}
+//     }
+// })
 
 client.on("messageCreate", async (message) => {
     if (message.author.id === botId) {
-        if (message.content.startsWith("Did you think I would @ everyone?")) {
-            setTimeout(async () => {
-                if (message.editedTimestamp !== message.createdTimestamp) {
-                    messageMap[message.id] = await message.reply(
-                        "Ok, I'll do it for you. @everyone",
-                    )
-                }
-            }, 5000)
-        } else {
-            if (message.deletable) {
-                try {
-                    await message.delete()
-                } catch {}
-            }
+        // if (message.content.startsWith("Did you think I would @ everyone?")) {
+        //     setTimeout(async () => {
+        //         if (message.editedTimestamp !== message.createdTimestamp) {
+        //             messageMap[message.id] = await message.reply(
+        //                 "Ok, I'll do it for you. @everyone",
+        //             )
+        //         }
+        //     }, 5000)
+        // } else {
+        //     if (message.deletable) {
+        //         try {
+        //             await message.delete()
+        //         } catch {}
+        //     }
+        // }
+        if (message.deletable) {
+            try {
+                await message.delete()
+            } catch {}
         }
     }
 })
